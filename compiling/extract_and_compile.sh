30db99fdf2103837ce0354b016af1090c6d4cfae
M1NN <score@morris.umn.edu>
2018-09-04 10:05:02 -0500

    Final Version
    Fixed the printing issues that caused tests to fail

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,12 +4,12 @@ Number=$1
 Directory=$2
 Location='/home/score022/Documents/csci3403/lab-0-git-shell-programming-intro-betterteamname/compiling/NthPrime.tgz'
 
-mkdir $Directory
-tar xvfz NthPrime.tgz -C $Directory
+#mkdir $Directory
+tar xfz NthPrime.tgz -C $Directory
 
 cd $Directory
 cd NthPrime
 
-gcc -Wall nth_prime.h nth_prime.c main.c -o NthPrime
+gcc -Wall nth_prime.h main.c nth_prime.c -o NthPrime
 
 ./NthPrime $Number
